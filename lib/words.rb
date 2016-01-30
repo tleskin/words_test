74c186fc803579cd5683f82fc81d56a4b24d5c37
Tom Leskin <tleskin@gmail.com>
2016-01-30 15:17:07 -0700

    Word class can find and return an array of all sequences

--- lib/words.rb
+++ lib/words.rb
@@ -6,11 +6,28 @@ class Words
   def initialize(dictionary)
     @parser = Parser.new
     @to_analyze = read_file(dictionary)
-    @sequences = 'sequences.txt'
+    @sequences = []
     @words = 'words.txt'
   end
 
   def read_file(dictionary)
     @parser.parse(dictionary)
+  end
+
+  def get_sequences
+    @to_analyze.each do |word|
+
+      next if word.length <= 3
+
+      word.chars.each_cons(4) do |seq_arr|
+        sequence = seq_arr.join
+
+        next unless sequence =~ /\A[A-Za-z]+\z/
+
+        next if sequences.include?(sequence)
+
+        sequences << sequence
+      end
+    end
   end
 end
--- test/words_test.rb
+++ test/words_test.rb
@@ -17,4 +17,18 @@ class WordsTest < MiniTest::Test
     assert_equal ["arrows", "carrots", "give", "me"], output
     assert_equal Array, output.class
   end
+
+  def test_it_can_return_an_array_of_sequences_for_the_words
+    @worder.get_sequences
+    expected_sequences = ["arro", "rrow", "rows", "carr", "rrot", "rots", "give"]
+    assert_equal Array, @worder.sequences.class
+    assert_equal expected_sequences, @worder.sequences
+  end
+
+  def test_a_sequences_is_only_added_to_the_array_once
+    @worder.get_sequences
+    selected_sequences = @worder.sequences.select {|sequence| sequence == "arro"}
+    refute_equal 2, selected_sequences.length
+    assert_equal 1, selected_sequences.length
+  end
 end
